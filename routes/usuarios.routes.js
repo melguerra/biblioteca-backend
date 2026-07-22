6830bd8e18168b57a32390d6e6a64a26a0c770f9
melguerra <melanigisel.guerrajorge@alumnos.uai.edu.ar>
2026-07-22 11:20:59 -0300

    Se implementa autenticación con JWT

--- routes/usuarios.routes.js
+++ routes/usuarios.routes.js
@@ -1,5 +1,6 @@
 const express = require("express"); //importa express
 const Usuario = require("../models/Usuario"); //importa el modelo de usuario para poder usarlo en las rutas
+const jwt = require("jsonwebtoken"); //importa libreria para crear tokens de autenticacion
 
 const router = express.Router(); //crea un router de express para poder definir las rutas de usuarios
 
@@ -22,22 +23,38 @@ router.post("/registro", async (req, res) => {  //crea una ruta post llamada reg
 router.post("/login", async (req, res) => {
   try {
 
-    const { email, password } = req.body;
+    const { email, password } = req.body; //obtiene el mail y contraseña que envia el cliente
 
-    const usuario = await Usuario.findOne({ email });
+    const usuario = await Usuario.findOne({ email }); //aca busca en MogoDB un usuario con ese email
 
     if (!usuario) {
-      return res.status(404).json({ mensaje: "Usuario no encontrado" });
+      return res.status(404).json({ mensaje: "Usuario no encontrado" }); //si no existe devuelve error
     }
 
     if (usuario.password !== password) {
-      return res.status(401).json({ mensaje: "Contraseña incorrecta" });
+      return res.status(401).json({ mensaje: "Contraseña incorrecta" }); //si el email existe pero la contraseña no coincide entonces devuelve otro error
     }
 
-    res.json({
-      mensaje: "Login correcto",
-      usuario,
-    });
+// creamos el token JWT: donde guardaremos el id del usuario y email
+const token = jwt.sign(
+  {
+    id: usuario._id,
+    email: usuario.email,
+  },
+  process.env.JWT_SECRET, // usa la clave que agrege al archivo .env
+  {
+    expiresIn: "1h", //el token sera valido durante 1 hr, dsps tendra que iniciar sesion nuevamente.
+  }
+);    
+   res.json({
+  mensaje: "Login correcto",
+  token,
+  usuario: {
+    id: usuario._id,
+    nombre: usuario.nombre,
+    email: usuario.email,
+  },
+});
 
   } catch (error) {
     res.status(500).json({ mensaje: "Error al iniciar sesión" });
